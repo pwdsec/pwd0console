18e3bcb8bf8446266433c9fd8ebeb18988ec5c9f
pwd0kernel <pwd0kernel@gmail.com>
2021-04-20 16:49:38 -0230

    update

---
+++ source/malicious.py
@@ -0,0 +1,11 @@
+# File: malicious.py
+# Author: pwd0kernel
+# Date: 4/20/2021
+# Description: malicious/bugs/vulns commands
+
+import requests
+import warnings
+from bs4 import BeautifulSoup
+
+class Malicious():
+    pass
---
+++ source/misc.py
@@ -0,0 +1,11 @@
+# File: malicious.py
+# Author: pwd0kernel
+# Date: 4/20/2021
+# Description: Miscellaneous commands
+
+import requests
+import warnings
+from bs4 import BeautifulSoup
+
+class Miscellaneous():
+    pass
