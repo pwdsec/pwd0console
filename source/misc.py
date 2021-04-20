9cb2d8b245f5a64de69754025ebfa30df9157697
pwd0kernel <pwd0kernel@gmail.com>
2021-04-20 18:23:00 -0230

    Update misc.py
    added downloads lynx functions

--- source/misc.py
+++ source/misc.py
@@ -5,7 +5,32 @@
 
 import requests
 import warnings
+import wget
 from bs4 import BeautifulSoup
 
 class Miscellaneous():
-    pass
+    
+    def DownloadLynx():
+        try:
+            print("Downloading Lynx.zip...")
+            wget.download("https://lynx.rip/downloads/Lynx.zip", "Lynx.zip")
+            print("Downloaded. it should be in the same directory.")
+        except Exception:
+            print("There was an error downloading lynx.zip.")
+
+    
+    def DownloadLynxBackend():
+        try:
+            print("Downloading lynx_backend.zip...")
+            wget.download("https://lynx.rip/backend/downloads/Release.lynx", "lynx_backend.zip")
+            print("Downloaded. it should be in the same directory.")
+        except Exception:
+            print("There was an error downloading lynx_backend.zip")
+
+    def DownloadLynxDll():
+        try:
+            print("Downloading lynx.dll...")
+            wget.download("http://lynx.rip/backend/downloads/L09dahjd034.dll", "lynx.dll")
+            print("Downloaded. it should be in the same directory.")
+        except Exception:
+            print("There was an error downloading lynx.dll")
